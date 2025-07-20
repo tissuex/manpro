2858ef3546670ec34d99f21a9bfac23067b57270
tissuex <145247257+tissuex@users.noreply.github.com>
2025-07-20 17:18:27 +0700

    Delete UAS-PBO directory

--- UAS-PBO/perancangan/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profil, Proyek, Dokumen, Tugas, LaporanKemajuan
-
-# Mendaftarkan setiap model agar muncul di halaman admin
-admin.site.register(Profil)
-admin.site.register(Proyek)
-admin.site.register(Dokumen)
-admin.site.register(Tugas)
-admin.site.register(LaporanKemajuan)
